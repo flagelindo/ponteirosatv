ca223108e0370e33a59009a524ee031a704cebb4
flagelindo <116556349+flagelindo@users.noreply.github.com>
2024-10-22 16:19:29 -0300

    questao08

---
+++ questao08.c
@@ -0,0 +1,5 @@
+//8. 
+◦ *(pulo + 2);//correto, pois está atribuido ao valor de pulo e +2 significaria somar 2 além da posição 0, queria seria o 3 (valor de pulo[2])
+◦ *(pulo + 4); // incorreto, pois pulo está atribuido corretamente, mas o +4 não vai referenciar o terceiro elemento do vetor (valor de pulo [4])
+◦ pulo + 4; // incorreto, pois ele buscaria o endereço e não o valor
+◦ pulo + 2;//incorreto, pois ele buscaria o endereço e não o valor 
