9ebbbda9d461c2ad6dadec8860a84be945ac1544
flagelindo <116556349+flagelindo@users.noreply.github.com>
2024-10-25 10:47:56 -0300

    questao09.c

--- questao09.c
+++ questao09.c
@@ -1,1 +1,21 @@
-int mat[4], *p, x;p = mat + 1;p = mat++;p = ++mat;x = (*mat); 
+#include <stdio.h>
+
+int main() {
+    int mat[4] = {10, 20, 30, 40};
+    int *p;
+    int x;
+
+    // Expressão válida, mat recebe o endereço de ma[1]
+    p = mat + 1;
+    
+    // Expressão inválida, arrays são imutáveis em relação ao seu endereço inicial
+     p = mat++;
+
+    // Expressão inválida, a mesma coisa da questão anterior, mat não é uma variável, mas sim um bloco de memória constante
+     p = ++mat;
+
+    // Expressão válida, *mat desreferencia o ponteiro mat, que aponta para o primeiro elemento do array.
+    x = (*mat);
+
+    return 0;
+}
